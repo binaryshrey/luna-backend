d603afb9a14d17f58cdfafc910e0032c9e3f20e4
binaryshrey <shreyansh.saurabh01@gmail.com>
2025-11-25 10:36:43 -0800

    add health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.api import users, venues, reco, plans
+from app.api import users, venues, reco, plans, health
 
-app = FastAPI(title="Luna Backend Track 2")
+app = FastAPI(title="Luna API Service")
 
+app.include_router(health.router, prefix="/health", tags=["health"])
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(venues.router, prefix="/venues", tags=["venues"])
 app.include_router(reco.router, prefix="/reco", tags=["recommendations"])
